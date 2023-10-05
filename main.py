439b3367f7971989fa7a40a5f5850e50b80be465
Rozhkov Ivan <vanroz333@gmail.com>
2023-10-05 09:12:15 +0700

    sort

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import random
 
-a = list()
 N = int(input())
 
-for i in range(N):
-    a.append(random.randint(1, 20))
 
-print(a)
+def gen():
+    a = list()
+    N = int(input())
+    for i in range(N):
+        a.append(random.randint(1, 20))
+    return a
 
-for j in range(N - 1):
-    for i in range(N - j - 1):
-        if a[i] > a[i + 1]:
-            a[i], a[i + 1] = a[i + 1], a[i]
 
-print(a)
+def buble(a):
+    for j in range(N - 1):
+        for i in range(N - j - 1):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+    return a
+def sort(a):
+    n = len(a)
+    for i in range(n - 1):
+        m = i
+        for j in range(i + 1, n):
+            if a[j] < a[m]:
+                m = j
+            a[i], a[m] = a[m], a[i]
+    return a
 
+
+a = gen()
+print(sort(a))
 
 
